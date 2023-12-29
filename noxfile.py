0733eb017f6d2f5f0b57da3834b59e7193bf09e6
Scott Burns <scott@lentigo.net>
2023-12-29 13:40:53 -0500

    Set up test coverage reporting

--- noxfile.py
+++ noxfile.py
@@ -3,5 +3,6 @@ from nox_poetry import session
 
 @session(python=["3.10", "3.11", "3.12"])
 def test(session):
+    args = session.posargs or ["--cov"]
     session.install("pytest", "pytest-cov", ".")
-    session.run("pytest", "--cov")
+    session.run("pytest", *args)
